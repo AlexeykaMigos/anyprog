c08410fe3133e911b5f5a9a664a91c68ded17a70
Alexeyka Pavlov <alexeyka.migos@gmail.com>
2025-02-26 02:02:13 +0300

    adding structure

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 
-	r.GET("/api/products", getProducts)
-	r.POST("/api/products", addProduct)
+	//r.GET("/api/products", getProducts)
+	//r.POST("/api/products", addProduct)
 
-	r.Run(":8080")
+	err := r.Run(":8080")
+	if err != nil {
+		return
+	}
 }
