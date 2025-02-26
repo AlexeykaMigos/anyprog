b3808d618802e43ace54e079f4c428979414bb4e
Alexeyka Pavlov <alexeyka.migos@gmail.com>
2025-02-26 21:53:31 +0300

    adding db

--- main.go
+++ main.go
@@ -1,15 +1,51 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+	"log"
+)
 
 func main() {
+	connStr := "postgres://postgres:secret@localhost:5432/pgdb?sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+
+	defer db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	createTableProduct(db)
+
 	r := gin.Default()
 
-	//r.GET("/api/products", getProducts)
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
 	//r.POST("/api/products", addProduct)
 
-	err := r.Run(":8080")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal("error running server", err.Error())
+	}
+}
+
+func createTableProduct(db *sql.DB) {
+	query := `CREATE TABLE IF NOT EXISTS product (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    description TEXT
+	);`
+
+	_, err := db.Exec(query)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 }
